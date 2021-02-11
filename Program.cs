6e9737c6231b1a7da39913fb5eca80ba13d1b45c
Benjamin Macias <jacobmac10@hotmail.com>
2021-02-11 11:11:43 -0700

    se desarrollo el fizzbuzz

--- Program.cs
+++ Program.cs
@@ -5,8 +5,34 @@ namespace FizzBuzzGit
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        
+            {
+            for (int i = 0; i < 100; i++)
+            {
+                //Console.WriteLine(i);
+                if(i %3 == 0 && i %5 == 0)
+                {
+                  Console.WriteLine("FizzBuzz"); 
+                }
+                else if(i %3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if(i %5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else 
+                {
+                    Console.WriteLine(i);
+                }
+            
+            
+
+            
+            }
+        
         }
+        
     }
 }
